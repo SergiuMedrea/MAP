2053211e9ae050026a11c548a63da53c140c2d16
Medrea Sergiu <msergiu2015@gmail.com>
2023-11-09 11:44:17 +0200

    Finished Seminar 6

--- S6/src/main/java/ForecastDisplay.java
+++ S6/src/main/java/ForecastDisplay.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ForecastDisplay implements Observer, Display {
@@ -6,13 +7,15 @@ public class ForecastDisplay implements Observer, Display {
     private float airPressure;
     private int counter = 0;
     private List<Float> averageData;
+
     public ForecastDisplay(WeatherData weatherData) {
         weatherData.add(this);
+        averageData = new ArrayList<>();
     }
 
     @Override
     public void display() {
-        System.out.println("Forecast Conditions: " + temperature + " " + humidity + " " + airPressure);
+        System.out.println("Forecast Conditions: " + averageData.get(0) + " " + averageData.get(1) + " " + averageData.get(2));
     }
 
     @Override
@@ -29,10 +32,10 @@ public class ForecastDisplay implements Observer, Display {
             this.averageData.add(temperature);
             this.averageData.add(humidity);
             this.averageData.add(airPressure);
-//        } else {
-//            averageData.set(0, averageData.get(0) * counter + temperature) / (counter + 1);
-//            averageData.set(1, averageData.get(1) * counter + humidity) / (counter + 1);
-//            averageData.set(2, averageData.get(2) * counter + airPressure) / (counter + 1);
+        } else {
+            averageData.set(0, (averageData.get(0) * counter + temperature) / (counter + 1));
+            averageData.set(1, (averageData.get(1) * counter + humidity) / (counter + 1));
+            averageData.set(2, (averageData.get(2) * counter + airPressure) / (counter + 1));
         }
         counter += 1;
     }
--- S6/src/main/java/Main.java
+++ S6/src/main/java/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        WeatherData weatherData = new WeatherData();
-//        CurrentConditionDisplay currentConditionsDisplay = new CurrentConditionDisplay(weatherData);
-//        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-//        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionDisplay currentConditionsDisplay = new CurrentConditionDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
-        // Simulate changes in weather data
-//        weatherData.setMeasurements(75, 60, 30.4f);
-//        weatherData.setMeasurements(80, 65, 29.2f);
-//        weatherData.setMeasurements(72, 55, 29.5f);
+        weatherData.setMeasurements(75, 60, 30.4f);
+        weatherData.setMeasurements(80, 65, 29.2f);
+        weatherData.setMeasurements(72, 55, 29.5f);
 
         VGAMonitor vgaMonitor = new VGAMonitor();
         HDMIMonitor hdmiMonitor = new HDMIMonitor();
